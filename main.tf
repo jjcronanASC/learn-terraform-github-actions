629effb39b7710895f56effcbe71f8fd9313ab53
Jeremy Cronan <Jcronan@alliancesafetycouncil.org>
2022-05-27 14:43:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "alliancesafetycouncil
-"
+    organization = "alliancesafetycouncil"
 
     workspaces {
       name = "gh-actions-demo"
